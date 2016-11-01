db4892e402c49bcfa1f69ad7736ea2bb2eac558b
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-11-01 17:12:03 +0100

    squid 3.5.22

--- src/ubuntu16/scripts.squid3/05_build_squid.sh
+++ src/ubuntu16/scripts.squid3/05_build_squid.sh
@@ -11,25 +11,29 @@ rm -R build/squid3
 # we will be working in a subfolder make it
 mkdir -p build/squid3
 
-# copy the patch to the working folder
+# copy the patches to the working folder
 cp rules.patch build/squid3/rules.patch
+cp control.patch build/squid3/control.patch
 
 # decend into working directory
 pushd build/squid3
 
 # get squid3 from debian stretch
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.19-1.dsc
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.19.orig.tar.gz
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.19-1.debian.tar.xz
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.22-1.dsc
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.22.orig.tar.gz
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.22-1.debian.tar.xz
 
 # unpack the source package
-dpkg-source -x squid3_3.5.19-1.dsc
+dpkg-source -x squid3_3.5.22-1.dsc
 
 # modify configure options in debian/rules, add --enable-ssl --enable-ssl-crtd
-patch squid3-3.5.19/debian/rules < ../../rules.patch
+patch squid3-3.5.22/debian/rules < ../../rules.patch
+
+# modify control file, drop explicitly specified debhelper version
+patch squid3-3.5.22/debian/control < ../../control.patch
 
 # build the package
-cd squid3-3.5.19 && dpkg-buildpackage -rfakeroot -b
+cd squid3-3.5.22 && dpkg-buildpackage -rfakeroot -b
 
 # and revert
 popd
