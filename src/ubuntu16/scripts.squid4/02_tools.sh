ee4ad1c5ff43b2b5aae17de1b3bf6655c4d3ce09
Rafael Akchurin <rafael.akchurin@diladele.lan>
2018-02-07 22:27:47 +0100

    cleaning up

--- src/ubuntu16/scripts.squid4/02_tools.sh
+++ src/ubuntu16/scripts.squid4/02_tools.sh
@@ -8,14 +8,7 @@ fi
 # install build tools
 apt-get -y install devscripts build-essential fakeroot debhelper dh-autoreconf cdbs
 
-# install build dependences for squid
-# apt-get -y build-dep libecap
-# apt-get -y build-dep squid3
-
-# uninstall libecap2 as we will be using the libecap3
-# apt-get -y --purge remove libecap2-dev libecap2
-
-# install additional packages for new squid
+# install additional header packages for squid 4
 apt-get -y install \
 	libcppunit-dev \
 	libsasl2-dev \
@@ -29,11 +22,6 @@ apt-get -y install \
 	libpam0g-dev \
 	libgnutls28-dev \
 	libssl-dev
-
-nettle-dev  libssl-dev libdbi-perl \
-# 	      \
-# 	libkrb5-dev comerr-dev    \
-# 
 
 # reboot now
 reboot
