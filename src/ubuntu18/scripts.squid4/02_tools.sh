671d7738a8f11ea9ff24d374554b237159a7baa9
Rafael Akchurin <rafael.akchurin@diladele.lan>
2018-05-22 15:42:54 +0200

    added ecap to tools

--- src/ubuntu18/scripts.squid4/02_tools.sh
+++ src/ubuntu18/scripts.squid4/02_tools.sh
@@ -10,21 +10,21 @@ apt-get -y install devscripts build-essential fakeroot debhelper dh-autoreconf c
 
 # install additional header packages for squid 4
 apt-get -y install \
-	libcppunit-dev \
-	libsasl2-dev \
-	libxml2-dev \
-	libkrb5-dev \
-	libdb-dev \
-	libnetfilter-conntrack-dev \
-	libexpat1-dev \
-	libcap2-dev \
-	libldap2-dev \
-	libpam0g-dev \
-	libgnutls28-dev \
-	libssl-dev \
-	libdbi-perl \
-	libecap3 \
-	libecap3-dev
+    libcppunit-dev \
+    libsasl2-dev \
+    libxml2-dev \
+    libkrb5-dev \
+    libdb-dev \
+    libnetfilter-conntrack-dev \
+    libexpat1-dev \
+    libcap2-dev \
+    libldap2-dev \
+    libpam0g-dev \
+    libgnutls28-dev \
+    libssl-dev \
+    libdbi-perl \
+    libecap3 \
+    libecap3-dev
 
 # reboot now
 reboot
